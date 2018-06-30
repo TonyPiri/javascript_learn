e5433900878a94930b08d6e9bae9b9f5a7db9ee0
TonyPiri <lightyear1987@gmail.com>
2018-06-30 19:27:59 +0300

    fix 30.06

--- lessons/MDN/functions.js
+++ lessons/MDN/functions.js
@@ -3,3 +3,10 @@ https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Functions
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Functions
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/prototype
 */
+
+
+/*
+Функция - объект Function.
+
+-функция - не процедура. Она возвращает значение - либо undefined, либо знечение параметра this.
+-параметры вызова функции - это аргументы функции.
--- lessons/MDN/objects.js
+++ lessons/MDN/objects.js
@@ -1,3 +1,39 @@
 /*
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Working_with_Objects
 */
+
+/*
+Объекты - коллекция свойств и методов.
+Свойство - переменная, закрепленная за объектом.
+-Доступ к свойствам через точечную запись.
+*/
+
+var myCar = new Object();
+myCar.make = "Ford";
+myCar.model = "Mustang";
+myCar.year = 1969;
+
+//можно скобочной записью
+myCar["make"] = "Ford";
+myCar["model"] = "Mustang";
+myCar["year"] = 1969;
+
+/*
+Свойство - ключ(имя) - значение.
+Ключ - String.
+*/
+
+//Можно использовать переменную для доступа к свойству.
+var propertyName = "make";
+myCar[propertyName] = "Opel";
+
+function showProps(obj, objName) {
+    var result = "";
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            result += objName + "." + i + " = " + obj[i] + "\n";
+        }
+    }
+    return
+}
+showProps(myCar, "myCar")
